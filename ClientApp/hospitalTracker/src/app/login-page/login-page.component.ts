9ab10a76e65fbfaf44bc276a64afc48e7937dc65
Aria Sheikhtaheri <aria.staheri@gmail.com>
2020-06-04 22:13:40 -0400

    login page added

--- ClientApp/hospitalTracker/src/app/login-page/login-page.component.ts
+++ ClientApp/hospitalTracker/src/app/login-page/login-page.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  selector: "app-login-page",
+  templateUrl: "./login-page.component.html",
+  styleUrls: ["./login-page.component.scss"],
 })
 export class LoginPageComponent implements OnInit {
+  username = new FormControl("", [Validators.required]);
+  email = new FormControl("", [Validators.required, Validators.email]);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  getErrorMessage() {
+    if (this.username.hasError("required")) {
+      return "You must enter a value for username";
+    }
   }
 
+  getEmailErrorMessage() {
+    if (this.email.hasError("required")) {
+      return "Email is required!";
+    }
+
+    return this.email.hasError("email") ? "Not a valid email" : "";
+  }
 }
