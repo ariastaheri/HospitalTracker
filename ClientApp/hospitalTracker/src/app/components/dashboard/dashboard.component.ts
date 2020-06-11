c59aeb7f82e3ae87eeaeecf11865f93bd0c69111
Aria Sheikhtaheri <aria.staheri@gmail.com>
2020-06-11 00:42:44 -0400

    dashboard layout fixed

--- ClientApp/hospitalTracker/src/app/components/dashboard/dashboard.component.ts
+++ ClientApp/hospitalTracker/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Patient } from ".";
+import { MatTableDataSource } from "@angular/material/table";
+import { CloseScrollStrategy } from "@angular/cdk/overlay";
 
 @Component({
   selector: "app-dashboard",
@@ -9,66 +11,76 @@ import { Patient } from ".";
 export class DashboardComponent implements OnInit {
   constructor() {}
 
-  patients: Array<Patient>;
+  patients: Patient[] = [
+    {
+      _id: "1",
+      name: "Aria Taheri",
+      gender: "male",
+      visits: [],
+      DOB: new Date(1991, 8, 17),
+      history: "no history for this patient",
+    },
+    {
+      _id: "2",
+      name: "Amin Taheri",
+      visits: [],
+      gender: "male",
+      DOB: new Date(1997, 4, 24),
+      history: "History of mental illness",
+    },
+    {
+      _id: "3",
+      name: "Amin Taheri1",
+      visits: [],
+      gender: "male",
+      DOB: new Date(1997, 4, 24),
+      history: "History of mental illness",
+    },
+    {
+      _id: "4",
+      name: "Amin Taheri2",
+      gender: "male",
+      visits: [],
+      DOB: new Date(1997, 4, 24),
+      history: "History of mental illness",
+    },
+    {
+      _id: "1",
+      name: "Aria Taheri",
+      gender: "male",
+      visits: [],
+      DOB: new Date(1991, 8, 17),
+      history: "no history for this patient",
+    },
+    {
+      _id: "2",
+      name: "Amin Taheri",
+      gender: "male",
+      visits: [],
+      DOB: new Date(1997, 4, 24),
+      history: "History of mental illness",
+    },
+    {
+      _id: "5",
+      name: "Ghazale G",
+      gender: "female",
+      visits: [],
+      DOB: new Date(1991, 5, 4),
+      history: "No History",
+    },
+  ];
+
+  filteredPatients: Patient[];
 
   ngOnInit() {
-    this.patients = [
-      {
-        _id: "1",
-        name: "Aria Taheri",
-        gender: "male",
-        visits: [],
-        DOB: new Date(1991, 8, 17),
-        history: "no history for this patient",
-      },
-      {
-        _id: "2",
-        name: "Amin Taheri",
-        visits: [],
-        gender: "male",
-        DOB: new Date(1997, 4, 24),
-        history: "History of mental illness",
-      },
-      {
-        _id: "3",
-        name: "Amin Taheri1",
-        visits: [],
-        gender: "male",
-        DOB: new Date(1997, 4, 24),
-        history: "History of mental illness",
-      },
-      {
-        _id: "4",
-        name: "Amin Taheri2",
-        gender: "male",
-        visits: [],
-        DOB: new Date(1997, 4, 24),
-        history: "History of mental illness",
-      },
-      {
-        _id: "1",
-        name: "Aria Taheri",
-        gender: "male",
-        visits: [],
-        DOB: new Date(1991, 8, 17),
-        history: "no history for this patient",
-      },
-      {
-        _id: "2",
-        name: "Amin Taheri",
-        gender: "male",
-        visits: [],
-        DOB: new Date(1997, 4, 24),
-        history: "History of mental illness",
-      },
-      {
-        _id: "5",
-        name: "Ghazale G",
-        gender: "female",
-        visits: [],
-        DOB: new Date(1991, 5, 4),
-        history: "No History",
-      },
-    ];
+    this.filteredPatients = this.patients;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value.toLowerCase();
+    console.log(filterValue);
+    this.filteredPatients = this.patients.filter((patient) => {
+      return patient.name.toLowerCase().indexOf(filterValue) > -1;
+    });
   }
 }
